a5991cb39668a79d42a448f8d31d7e9664e2ebfe
Li-tozero <1347367399@qq.com>
2018-08-19 17:09:09 +0800

    add city-ajax

--- src/pages/city/components/alphabet.vue
+++ src/pages/city/components/alphabet.vue
@@ -1,27 +1,26 @@
 <template>
   <ul class="list">
-    <li class="item">A</li>
-    <li class="item">B</li>
-    <li class="item">A</li>
-    <li class="item">B</li>
-    <li class="item">A</li>
-    <li class="item">B</li>
-    <li class="item">A</li>
-    <li class="item">B</li>
-    <li class="item">A</li>
-    <li class="item">B</li>
-    <li class="item">A</li>
-    <li class="item">B</li>
+    <li
+      class="item"
+      v-for="(item,key) of cities"
+      :key="key"
+    >
+      {{key}}
+    </li>
   </ul>
 </template>
 
 <script type="text/ecmascript-6">
 export default {
-  name: 'CityAlphabet'
+  name: 'CityAlphabet',
+  props: {
+    cities: Object
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
   .list
     display: flex
     flex-direction: column
