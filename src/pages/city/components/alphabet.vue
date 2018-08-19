37d6dacde8a713416fc70445a59e0e61f1573d14
Li-tozero <1347367399@qq.com>
2018-08-19 16:36:26 +0800

    add css

---
+++ src/pages/city/components/alphabet.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">B</li>
+    <li class="item">A</li>
+    <li class="item">B</li>
+    <li class="item">A</li>
+    <li class="item">B</li>
+    <li class="item">A</li>
+    <li class="item">B</li>
+    <li class="item">A</li>
+    <li class="item">B</li>
+    <li class="item">A</li>
+    <li class="item">B</li>
+  </ul>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .list
+    display: flex
+    flex-direction: column
+    justify-content:center
+    position: absolute
+    top: 1.58rem
+    right: 0
+    bottom: 0
+    width: .4rem
+    /*background: red*/
+   .item
+    line-height: .4rem
+    text-align: center
+    color: $bgColor
+</style>
