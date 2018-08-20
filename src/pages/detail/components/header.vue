627f028dcac9a31b085843f75a8d18d30ff8e926
Li-tozero <1347367399@qq.com>
2018-08-20 11:33:52 +0800

    fix problem

--- src/pages/detail/components/header.vue
+++ src/pages/detail/components/header.vue
@@ -47,6 +47,10 @@ export default {
   },
   activated () {
     window.addEventListener('scroll', this.handleScroll)
+  },
+  // 对事件进行解绑
+  deactivated () {
+    window.removeEventListener('scroll', this.handleScroll)
   }
 }
 </script>
