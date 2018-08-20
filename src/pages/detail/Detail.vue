ae5eab314765f2edcc9e9ca67e9a188bf9b12bf9
Li-tozero <1347367399@qq.com>
2018-08-20 14:24:04 +0800

    finish detail-list

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,18 +2,53 @@
   <div>
     <DetailBanner></DetailBanner>
     <DetailHeader></DetailHeader>
-    <div class="content"></div>
+    <div class="content">
+      <DetailList :list="list"></DetailList>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
 import DetailBanner from './components/banner.vue'
 import DetailHeader from './components/header.vue'
+import DetailList from './components/list.vue'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [
+        {
+          title: '成人票',
+          children: [
+            {
+              title: '成人三级馆',
+              children: [
+                {
+                  title: '成人三级馆——某连锁店销售'
+                }
+              ]
+            },
+            {
+              title: '成人五级馆'
+            }
+          ]
+        },
+        {
+          title: '学生票'
+        },
+        {
+          title: '儿童票'
+        },
+        {
+          title: '特惠票'
+        }
+      ]
+    }
   }
 }
 </script>
